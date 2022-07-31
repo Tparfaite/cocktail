a456bce974c1fa267aca3f0758820197c25ff199
Tparfaite <parfaitetwagira@gmail.com>
2022-07-31 18:07:14 +0200

    Feat:initial commit -Add all [Finishes#]

---
+++ index.js
@@ -0,0 +1,3 @@
+// fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+// .then((data)=>data.json())
+// .then((display)=>display.map((item)=>document.getElementById("drinks").innerHTML)+=`${item.idDrink}`);
