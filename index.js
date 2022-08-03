3e38dd6363d19030989485c30758f0628782e3dc
Tparfaite <parfaitetwagira@gmail.com>
2022-08-03 09:32:48 +0200

    Feat:first commit -Add all [finishes#]

--- index.js
+++ index.js
@@ -1,3 +1,71 @@
-// fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
-// .then((data)=>data.json())
-// .then((display)=>display.map((item)=>document.getElementById("drinks").innerHTML)+=`${item.idDrink}`);
+const drinks=document.getElementById("drinks");
+
+
+
+
+const displayData=(label,src)=>{
+
+const card=`
+
+<div class="card">
+             <div class="label">
+              ${label}
+             </div>
+             <img src=${src}>
+           </div>
+
+`
+drinks.innerHTML+=card;
+
+}
+displayData();
+
+document.addEventListener("DOMContentLoaded",e=>{
+
+    const fetchData=async()=>{
+
+        const items=await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+        .then((data)=>data.json())
+        .then((display)=>display.drinks);
+        // console.log(items);
+        // items.map((item)=>console.log(item.strDrinkThumb))
+        // items.map((item)=>console.log(item.strDrink))
+        items.map(item=>displayData(item.strDrink,item.strDrinkThumb))
+        }
+        fetchData();
+
+
+        // const search1=document.getElementById("search1");
+
+        // search1.addEventListener("keyup",e=>{
+        
+        // const bring=fetchData.map(item=>{
+        // console.log(bring);
+        
+        // })
+        // bring();
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
