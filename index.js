2a585b1f1ca1484765a7756206be08ea9be3038a
Tparfaite <parfaitetwagira@gmail.com>
2022-08-03 09:37:42 +0200

    Feat:map array -Add all [Finishes#]

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ document.addEventListener("DOMContentLoaded",e=>{
         const items=await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
         .then((data)=>data.json())
         .then((display)=>display.drinks);
-        // console.log(items);
-        // items.map((item)=>console.log(item.strDrinkThumb))
-        // items.map((item)=>console.log(item.strDrink))
+        console.log(items);
+        items.map((item)=>console.log(item.strDrinkThumb))
+        items.map((item)=>console.log(item.strDrink))
         items.map(item=>displayData(item.strDrink,item.strDrinkThumb))
         }
         fetchData();
